34aff1c5975033b1817daf5ebec0b27c55ac8828
sajib689 <jibonraja11@gmail.com>
2023-08-26 10:31:15 +0600

    login panel has been update

--- src/Authntication/PrivateRoute/PrivateRoute.jsx
+++ src/Authntication/PrivateRoute/PrivateRoute.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
-
-const PrivateRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+import React, {useContext} from 'react';
+import { AuthContext } from "../../Providers/AuthProviders";
+import { Navigate } from 'react-router-dom';
+import { ThreeDots } from 'react-loader-spinner';
+const PrivateRoute = ({children}) => {
+    const {user, loading} = useContext(AuthContext)
+    if(loading) {
+    return <ThreeDots 
+    height="80" 
+    width="80" 
+    radius="9"
+    color="#4fa94d" 
+    ariaLabel="three-dots-loading"
+    wrapperStyle={{}}
+    wrapperClassName=""
+    visible={true}
+     />
+    }
+   if(user) {
+    return children
+   }
+   return <Navigate to='/login'></Navigate>
 };
 
 export default PrivateRoute;
--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -5,7 +5,9 @@ export const AuthContext = createContext(null)
 const auth = getAuth(app)
 const AuthProviders = ({children}) => {
     const [user, setUser] = useState('')
+    const [loading, setLoading] = useState(true)
     const formSign = ( email, password, name, file) => {
+       
         return createUserWithEmailAndPassword(auth, email, password, name, file)
         .then( result => {
             const user = result.user
@@ -16,15 +18,18 @@ const AuthProviders = ({children}) => {
         })
     }
     const login = (email, password) => {
+    
         return signInWithEmailAndPassword(auth, email, password)
 
     }
     const logOut = () => {
+      
         return signOut(auth)
     }
     useEffect(() => {
       const unsubscribe =  onAuthStateChanged(auth, currentUser => {
         setUser(currentUser)
+        setLoading(false)
         })
         return () => {
             unsubscribe()
@@ -34,7 +39,8 @@ const AuthProviders = ({children}) => {
         user,
         formSign,
         login,
-        logOut
+        logOut,
+        loading
     }
     return (
         <AuthContext.Provider value={authInfo}>
